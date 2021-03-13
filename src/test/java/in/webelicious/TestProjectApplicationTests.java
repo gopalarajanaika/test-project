1b4bf3913b4c9893badb385a46d3054ac5929236
Gopal <gopalanaika@infrrd.ai>
2021-03-13 14:35:17 +0530

    all files commit

---
+++ src/test/java/in/webelicious/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package in.webelicious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
